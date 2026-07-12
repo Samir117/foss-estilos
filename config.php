8d7d6c601e96d396be0f97a71b56540360456e0b
Samir Rojas Acosta <samirrojas42@gmail.com>
2026-07-11 19:06:13 -0500

    estructura de los estilos

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'brand' => [
+        'name'        => 'Ciudad Creativa',
+        'primary_color' => '#f22f2f',
+    ],
+    'whitelabel' => [
+        'hide_fossbilling_branding' => true,
+        'hide_powered_by'          => true,
+    ],
+    'assets' => [
+        'css' => [
+            'estilos' => 'assets/css/estilos.css',
+        ],
+    ],
+];
